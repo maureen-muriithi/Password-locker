856ca00abb0cb97456af9442db44ba0e5cefa5c6
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-23 17:51:11 +0300

    Add init method that allows us to pass properties for the new object

--- password_user.py
+++ password_user.py
@@ -1,5 +1,15 @@
-class user:
+class User:
     '''
     A class that generates new instances for a user.
     '''
-    pass
+    user_list = [] # Empty user list
+
+    def __init__(self, user_name, password):
+        '''
+        init method that defines properties for the user class
+        Args: user_name, password
+        '''
+        self.user_name = user_name
+        self.password = password
+    
+    
